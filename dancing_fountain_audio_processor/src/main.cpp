dfc3adb999a1288b2a30606f904e6ca0dbbd3b41
PeterEmad146 <peteremads1406@gmail.com>
2025-04-13 22:59:47 +0200

    main.cpp uploaded for the test of the functionality of the file_utils.cpp file

--- dancing_fountain_audio_processor/src/main.cpp
+++ dancing_fountain_audio_processor/src/main.cpp
@@ -1,1 +1,18 @@
-// Main application entry point
+#include <iostream>
+#include "file_utils.h"  // Include the header for file validation functions
+
+int main() {
+    std::cout << "🎧 MP3 File Validator\n";
+    std::cout << "----------------------\n";
+
+    // Prompt the user for an MP3 file path and validate it
+    std::string validMP3Path = file_utils::prompt_and_validate_mp3();
+
+    // At this point, the file has been verified
+    std::cout << "\n📂 File Ready for Use: " << validMP3Path << "\n";
+
+    // You can now proceed to do something with the valid MP3 file
+    // e.g., add to a playlist, copy to another directory, etc.
+
+    return 0;
+}
